9238168118b9412d82889480a985c5369ecbc53d
Ayazhan98 <aiko.06.98@mail.ru>
2019-06-09 21:45:36 +0600

    without co-authors

--- scopus1/views.py
+++ scopus1/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+
 from .models import  *
 
 
 def paper (request):
     obj3 = Paper.objects.all()
+    o=Authored.objects.all().prefetch_related ('p_title')
     obj1 = Authors.objects.all()
     obj2 = P_type.objects.all()
-
     obj4=Authored.objects.all()
 
-    return render(request, 'scopus1/index.html', context={'authors':obj1,'paper':obj3,'ptype':obj2,'authored':obj4})
+
+    return render(request, 'scopus1/index.html', context={'authors':obj1,'paper':obj3,'ptype':obj2,'authored':o})
