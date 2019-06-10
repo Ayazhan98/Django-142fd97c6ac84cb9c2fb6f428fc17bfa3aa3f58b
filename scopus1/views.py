4e58da8a6546e3f997fe0fda97350afb19b46648
Ayazhan98 <aiko.06.98@mail.ru>
2019-06-10 16:08:08 +0600

    with red button

--- scopus1/views.py
+++ scopus1/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-
-from .models import  *
+from .models import *
 
 
 def paper (request):
-    obj3 = Paper.objects.all()
-    o=Authored.objects.all().prefetch_related ('p_title')
-    obj1 = Authors.objects.all()
-    obj2 = P_type.objects.all()
-    obj4=Authored.objects.all()
-
-
-    return render(request, 'scopus1/index.html', context={'authors':obj1,'paper':obj3,'ptype':obj2,'authored':o})
+    publications = Paper.objects.all()
+    authored = Authored.objects.all()
+    authors = Authors.objects.all()
+    publication_types = P_type.objects.all()
+    authored1 = Authored.objects.all().order_by('author')
+    return render(request, 'scopus1/index.html', context={'authors':authors,'paper':publications,'authored1':authored1 ,'authored':authored})
